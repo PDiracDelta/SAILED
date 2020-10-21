47d00d34042f3c295dee20ba8cb9026bd4df52d3
Joris VAN HOUTVEN <joris.vanhoutven@uhasselt.be>
2020-10-21 10:42:30 +0200

    add moderated_ttest and get_design_matrix to other_functions.R

--- other_functions.R
+++ other_functions.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 # convert data to long format (assume Run, Mixture columns already present)
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
@@ -148,3 +149,70 @@ conf.mat <- function(dat, score.var,cut.off){
 # cm <- conf.mat(dat.dea[[1]], 'q.mod', contrast.names, 0.05)
 # xtable(cm$tab, type='html')
 # xtable(cm$metrics, type='html')
+
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+# get design matrix for use in moderated t-test
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
+get_design_matrix <- function(referenceCondition, study.design) {
+  # ANOVA-like design matrix for use in moderated_ttest, indicating group 
+  # (condition) membership of each entry in all_channels.
+  
+  # remove internal reference cols if still present
+  study.design <- study.design[!(study.design$Channel %in% c('126', '131')),]
+  
+  otherConditions = setdiff(unique(study.design$Condition), referenceCondition)
+  study.design.unitedchannel <- study.design %>% unite(Channel, c(Channel,Run))
+  all_channels = study.design.unitedchannel %>% select(Channel) %>% pull
+  N_channels = length(all_channels) 
+  N_conditions = 1+length(otherConditions)
+  design = matrix(rep(0,N_channels*N_conditions), c(N_channels, N_conditions))
+  design[, 1] <- 1  # reference gets 1 everywhere
+  rownames(design) <- all_channels
+  colnames(design) <- c(referenceCondition, otherConditions)
+  for (i in 1:N_channels) {  # for each channel in each condition, put a "1" in the design matrix.
+    design[study.design.unitedchannel$Channel[i], study.design.unitedchannel$Condition[i]] = 1
+  }
+  return(design)
+}
+
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+# moderated t-test for data-driven approach
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
+moderated_ttest <- function(dat, design, scale='raw') {
+  library(limma)
+  select.scale=match.arg(scale, c('log', 'raw'))
+  if (select.scale=='log'){
+  # This version makes MAXIMAL use of limma. It is an extension of eb.fit, but 
+  # with terminology from moderated_ttest_extracted, which returns 
+  # output for more than 1 sample comparison.
+  ngenes <- dim(dat)[1]
+  fit <- eBayes(lmFit(dat, design))
+  logFC <- fit$coefficients # estimate of the log2-fold-change corresponding to the effect size
+  df.r <- fit$df.residual # residual degrees of freedom assiciated with ordinary t-statistic and p-value
+  df.0 <- rep(fit$df.prior, ngenes) # degrees of freedom associated with s2.0
+  s2.0 <- rep(fit$s2.prior, ngenes) # estimated prior value for the variance
+  s2 <- (fit$sigma)^2 # sample variance
+  s2.post <- fit$s2.post # posterior value for the variance
+  t.ord <- fit$coefficients / fit$stdev.unscaled / fit$sigma # vector of ordinary t-statistic: using sigma vs s2.post
+  t.mod <- fit$t # moderated t-statistic
+  p.ord <- 2*pt(-abs(t.ord), fit$df.residual) # ordinary p-value corresonding to the ordinary t-statistic
+  p.mod <- fit$p.value # moderated p-value corresonding to the moderated t-statistic
+  if(ngenes>1) q.ord <- apply(X = p.ord, MARGIN = 2, FUN = p.adjust, method='BH') # ordinary q-value corresponding to the ordinary t-statistic
+  # if(ngenes>1) q.ord <- qvalue(p.ord)$q#, pi0=1)$q # avoid qvalue library when using BH correction
+  else q.ord <- p.ord
+  if(ngenes>1) q.mod <- apply(X = p.mod, MARGIN = 2, FUN = p.adjust, method='BH') # moderated q-value corresponding to the moderated t-statistic
+  # if(ngenes>1) q.mod <- qvalue(p.mod)$q#, pi0=1)$q # avoid qvalue library when using BH correction
+  else q.mod <- p.mod
+  # incorporate entity type into colnames to overwrite identical factor names
+  colnames(logFC) <- paste0('logFC_', colnames(logFC))
+  colnames(t.ord) <- paste0('t.ord', '_', colnames(t.ord))
+  colnames(t.mod) <- paste0('t.mod', '_', colnames(t.mod))
+  colnames(p.ord) <- paste0('p.ord', '_', colnames(p.ord))
+  colnames(p.mod) <- paste0('p.mod', '_', colnames(p.mod))
+  colnames(q.ord) <- paste0('q.ord', '_', colnames(q.ord))
+  colnames(q.mod) <- paste0('q.mod', '_', colnames(q.mod))
+  results <- data.frame(logFC, t.ord, t.mod, p.ord, p.mod, q.ord, q.mod, df.r, df.0, s2.0, s2, s2.post)
+  rownames(results) <- rownames(dat)
+}
