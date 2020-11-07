cefed373c4caa67367a040c0a633208d8be5cb26
piotr.prostko <piotr.prostko@uhasselt.be>
2020-11-07 17:24:24 +0100

    add deqms_test

--- other_functions.R
+++ other_functions.R
@@ -406,3 +406,60 @@ permutation_test <- function(dat, referenceCondition, otherConditions, B=1000, s
   rownames(results) <- proteins
   return(results)
 }
+
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+# DEqMS
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+# wrapper function on the DEqMS method from the 'DEqMS' package.
+# psm.counts should be a dataframe with two columns: the first one containing protein names and 
+# the second one containing corresponding PSM counts
+
+deqms_test <- function(dat, design, scale, psm.counts) {
+  design <- design[match(colnames(dat), rownames(design)),]  # fix column order
+  ngenes <- dim(dat)[1]
+  fit <- eBayes(lmFit(dat, design))
+  ord=match(rownames(fit$coefficients), psm.counts[,1])
+  fit$count = psm.counts[ord,2]
+  fit = spectraCounteBayes(fit)
+  
+  logFC <- fit$coefficients # estimate of the log2-fold-change corresponding to the effect size
+  reference_condition <- colnames(design)[colSums(design) == nrow(design)]
+  reference_averages <- fit$coefficients[,reference_condition]  
+  if (scale!='log') {  # the inputs are RAW and NOT LOG2 transformed.
+    # therefore, coefficients are NOT log fold changes! --> Adjust
+    # fitted_response_matrix <- fit$coefficients %*% t(fit$design)
+    logFC <- log2((logFC+reference_averages)/reference_averages)
+    logFC[,reference_condition] <- logFC[,reference_condition] - 1  # correct reference
+  } else {   # still have to correct reference
+    logFC[,reference_condition] <- logFC[,reference_condition] - reference_averages }
+  df.r <- fit$df.residual # residual degrees of freedom assiciated with ordinary t-statistic and p-value
+  df.0 <- rep(fit$sca.dfprior, ngenes) # degrees of freedom associated with s2.0
+  s2.0 <- fit$sca.priorvar # estimated prior value for the variance
+  s2 <- (fit$sigma)^2 # sample variance
+  s2.post <- fit$sca.postvar	# posterior value for the variance
+  PSMcount <- fit$count
+  t.ord <- fit$coefficients / fit$stdev.unscaled / fit$sigma # vector of ordinary t-statistic: using sigma vs s2.post
+  t.mod <- fit$sca.t	 # moderated t-statistic
+  p.ord <- 2*pt(-abs(t.ord), fit$df.residual) # ordinary p-value corresonding to the ordinary t-statistic
+  p.mod <- fit$sca.p # moderated p-value corresonding to the moderated t-statistic
+  if(ngenes>1) q.ord <- apply(X = p.ord, MARGIN = 2, FUN = p.adjust, method='BH') # ordinary q-value corresponding to the ordinary t-statistic
+  # if(ngenes>1) q.ord <- qvalue(p.ord)$q#, pi0=1)$q # avoid qvalue library when using BH correction
+  else q.ord <- p.ord
+  if(ngenes>1) q.mod <- apply(X = p.mod, MARGIN = 2, FUN = p.adjust, method='BH') # moderated q-value corresponding to the moderated t-statistic
+  # if(ngenes>1) q.mod <- qvalue(p.mod)$q#, pi0=1)$q # avoid qvalue library when using BH correction
+  else q.mod <- p.mod
+
+  # incorporate entity type into colnames to overwrite identical factor names
+  colnames(logFC) <- paste0('logFC_', colnames(logFC))
+  colnames(t.ord) <- paste0('t.ord', '_', colnames(t.ord))
+  colnames(t.mod) <- paste0('t.mod', '_', colnames(t.mod))
+  colnames(p.ord) <- paste0('p.ord', '_', colnames(p.ord))
+  colnames(p.mod) <- paste0('p.mod', '_', colnames(p.mod))
+  colnames(q.ord) <- paste0('q.ord', '_', colnames(q.ord))
+  colnames(q.mod) <- paste0('q.mod', '_', colnames(q.mod))
+  results <- data.frame(logFC, t.ord, t.mod, p.ord, p.mod, q.ord, q.mod, df.r, df.0, s2.0, s2, s2.post, PSMcount)
+  rownames(results) <- rownames(dat)
+  # remove referenceCondition values; they are irrelevant
+  return(results %>% select(-contains(reference_condition)))
+}
+
