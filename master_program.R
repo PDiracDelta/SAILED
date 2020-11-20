42ae7babc63f80122e4837f4481aeece61898a6c
piotr.prostko <piotr.prostko@uhasselt.be>
2020-11-20 19:37:04 +0100

    seems like envir=new.env() is not helping in master_program

--- master_program.R
+++ master_program.R
@@ -1,16 +1,26 @@
-### knit all the notebooks specified below
-library(rmarkdown)
-st=Sys.time()
-#render("intro.Rmd") 
-render("datadriven_unit.Rmd", envir=new.env())
-render("datadriven_summarization.Rmd", envir=new.env())
-render("datadriven_normalization.Rmd", envir=new.env())
-render("datadriven_DEA.Rmd", envir=new.env())
-render("CONSTANd_vs_medianSweeping.Rmd", envir=new.env()) 
+clean.session <- function(){
+  # install remotes and download JGmisc from github
+  # install.packages(remotes)
+  # library(remotes)
+  # remotes::install_github("jasongraf1/JGmisc")
+  library(JGmisc)
+  detachAllPackages() # detach packages
+  library(rmarkdown)
+}
 
-render("modelbased_unit.Rmd", envir=new.env()) 
-render("modelbased_summarization.Rmd", envir=new.env())
-render("modelbased_normalization.Rmd", envir=new.env())
-render("modelbased_DEA.Rmd", envir=new.env())
+### knit all the notebooks specified below
+st=Sys.time()
+
+#clean.session(); rm(list = ls()[!ls()=='clean.session']); render("intro.Rmd") 
+clean.session(); rm(list = ls()[!ls()=='clean.session']); render("datadriven_unit.Rmd")
+clean.session(); rm(list = ls()[!ls()=='clean.session']); render("datadriven_summarization.Rmd")
+clean.session(); rm(list = ls()[!ls()=='clean.session']); render("datadriven_normalization.Rmd")
+clean.session(); rm(list = ls()[!ls()=='clean.session']); render("datadriven_DEA.Rmd")
+clean.session(); rm(list = ls()[!ls()=='clean.session']); render("CONSTANd_vs_medianSweeping.Rmd") 
+
+clean.session(); rm(list = ls()[!ls()=='clean.session']); render("modelbased_unit.Rmd") 
+clean.session(); rm(list = ls()[!ls()=='clean.session']); render("modelbased_summarization.Rmd")
+clean.session(); rm(list = ls()[!ls()=='clean.session']); render("modelbased_normalization.Rmd")
+clean.session(); rm(list = ls()[!ls()=='clean.session']); render("modelbased_DEA.Rmd")
 ed=Sys.time()
 ed-st # running time
