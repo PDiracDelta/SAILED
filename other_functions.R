db9f1638660818aedfdac111dd598346e876145d
j <xtrjoris@gmail.com>
2021-01-30 12:28:03 +0100

    bugfixes in moderated_ttest in other_functions.R

--- other_functions.R
+++ other_functions.R
@@ -328,12 +328,12 @@ moderated_ttest <- function(dat, design, scale) {
   t.mod <- fit$t # moderated t-statistic
   p.ord <- 2*pt(-abs(t.ord), fit$df.residual) # ordinary p-value corresonding to the ordinary t-statistic
   p.mod <- fit$p.value # moderated p-value corresonding to the moderated t-statistic
-  if(ngenes>1) q.ord <- apply(X = p.ord, MARGIN = 2, FUN = p.adjust, method='BH') # ordinary q-value corresponding to the ordinary t-statistic
+  if(ngenes>1) { q.ord <- apply(X = p.ord, MARGIN = 2, FUN = p.adjust, method='BH') # ordinary q-value corresponding to the ordinary t-statistic
   # if(ngenes>1) q.ord <- qvalue(p.ord)$q#, pi0=1)$q # avoid qvalue library when using BH correction
-  else q.ord <- p.ord
-  if(ngenes>1) q.mod <- apply(X = p.mod, MARGIN = 2, FUN = p.adjust, method='BH') # moderated q-value corresponding to the moderated t-statistic
+  } else q.ord <- p.ord
+  if(ngenes>1) { q.mod <- apply(X = p.mod, MARGIN = 2, FUN = p.adjust, method='BH') # moderated q-value corresponding to the moderated t-statistic
   # if(ngenes>1) q.mod <- qvalue(p.mod)$q#, pi0=1)$q # avoid qvalue library when using BH correction
-  else q.mod <- p.mod
+  } else q.mod <- p.mod
   
   # incorporate entity type into colnames to overwrite identical factor names
   colnames(logFC) <- paste0('logFC_', colnames(logFC))
@@ -346,13 +346,13 @@ moderated_ttest <- function(dat, design, scale) {
   results <- data.frame(logFC, t.ord, t.mod, p.ord, p.mod, q.ord, q.mod, df.r, df.0, s2.0, s2, s2.post)
   rownames(results) <- rownames(dat)
   # remove referenceCondition values; they are irrelevant
-  return(results %>% select(-contains(reference_condition)))
+  return(results %>% select(-contains(reference_condition, ignore.case = F)))
 }
 
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
 # Wilcoxon rank-sum test
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
-# Wilcoxon rank-sum test (equivalent to Mann–Whitney U test) comparing outcomes between 
+# Wilcoxon rank-sum test (equivalent to Mann?Whitney U test) comparing outcomes between 
 # two independent groups of samples. For each protein separately, the test is applied to each otherCondition w.r.t. referenceCondition.
 # Wilcoxon rank-sum test employs ranks of quantification values, therefore logFC must be computed manually:
 #   if logFC.method='diff': difference of arithmetic means computed on log2 scale
